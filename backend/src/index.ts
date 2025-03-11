16ce6dd8a3d0970d551b33f6dd3256ca265ca78a
Djennkins <djennkins228@gmail.com>
2025-03-11 13:55:22 +0200

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import recipeRoutes from "./routes/recipeRoutes";
 
+import cors from 'cors';
+
 dotenv.config();
 
 const app = express();
 const port = 5000;
+
+app.use(cors());
 
 app.use('/api', recipeRoutes);
 
