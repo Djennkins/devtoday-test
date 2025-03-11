1bb34d1bfddc182476eb2ca3bafad05077f45431
Djennkins <djennkins228@gmail.com>
2025-03-12 00:51:47 +0200

    added formatting of ingredients in getRecipeInfo controller function

--- backend/src/controllers/recipeController.ts
+++ backend/src/controllers/recipeController.ts
@@ -18,7 +18,31 @@ export const getAvailableRecipes = async (filterBy: string = '', filterValue: st
 export const getRecipeInfo = async (id: string) => {
     try {
         const response = await axios.get(`${process.env.RECIPE_API_BASE_URL}/lookup.php?i=${id}`);
-        return response.data;
+        const recipe = response.data.meals[0];
+
+        const ingredients = [];
+
+        for (let i = 1; i <= 20; i++) {
+            const ingredient = recipe[`strIngredient${i}`];
+            const measure = recipe[`strMeasure${i}`];
+            if (ingredient && ingredient.trim() && measure) {
+                ingredients.push({ ingredient, measure });
+            }
+        }
+
+        return  {
+            idMeal: recipe.idMeal,
+            strMeal: recipe.strMeal,
+            strCategory: recipe.strCategory,
+            strArea: recipe.strArea,
+            strInstructions: recipe.strInstructions,
+            strMealThumb: recipe.strMealThumb,
+            strTags: recipe.strTags,
+            strYoutube: recipe.strYoutube,
+            ingredients,
+            strImageSource: recipe.strImageSource,
+        };
+
     } catch (error) {
         console.error(error);
         return { error: 'Failed to fetch recipe information' };
